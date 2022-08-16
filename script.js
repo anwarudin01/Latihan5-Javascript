8fa836070ae07a245fbbfd0c3585f8b0c9115dfe
Muhammad Anwarudin <anwarudin.muhamad@yahoo.co.id>
2022-08-16 22:00:04 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+for (var i = 1; i <= 10; i++) {
+  if (i <= 6) {
+    console.log('angkot No ' + i + ' bekerja dengan baik');
+  } else if (i === 8) {
+    console.log('angkot No ' + i + ' sedang lembur');
+  } else {
+    console.log('angkot No ' + i + ' sedang tidak beroperasi');
+  }
+}
